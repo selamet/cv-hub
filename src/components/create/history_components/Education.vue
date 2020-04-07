2f3fecd02db0ef3fdb0831084b1e1cae0b97d5ab
selametsamli <selamet96@gmail.com>
2020-04-07 20:57:56 +0300

    Eğitim componenti eklendi

--- src/components/create/history_components/Education.vue
+++ src/components/create/history_components/Education.vue
@@ -1,9 +1,23 @@
 <template>
 
   <div class="col-10 offset-1 pt-3  mt-5 shadow ">
-    <h3>Eğitim ve Nitelikler</h3>
+    <h3>Eğitim ve Nitelikler ({{educationList.length}}) </h3>
 
-    <form>
+
+    <div v-if="educationList.length>0" class="show-education">
+      <ul class="list-group">
+        <li v-for="(ed,index) in educationList" class="list-group-item">
+
+          <div class=" float-left">{{ed.school}} {{index}}</div>
+          <div class=" float-right control-panel">
+            <button @click="destroyEducation(index)" class=" btn btn-outline-dar btn-sm">X</button>
+            <button @click="editEducation(index)" class=" btn btn-outline-dar btn-sm">D</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+    <form v-if="formShow">
       <div class="form-row">
         <div class="col-md-6 mb-3">
           <label>Derece</label>
@@ -25,14 +39,18 @@
           <br>
           <div class="col-md-6 float-left">
             <select v-model="education.starter_date.month" class="form-control ">
-              <option value="" selected>Month</option>
-              <option value="" v-for="month in months">{{month}}</option>
+              <option disabled selected>Month</option>
+              <option :value="'gosterme'">Gösterme</option>
+
+              <option :value="month" v-for="month in months">{{month}}</option>
             </select>
           </div>
           <div class="col-md-6 float-right">
             <select v-model="education.starter_date.year" class="form-control ">
-              <option value="" selected>Year</option>
-              <option value="" v-for="i in range(1960, 2021)">{{i}}</option>
+              <option disabled selected>Year</option>
+              <option :value="'gosterme'">Gösterme</option>
+
+              <option :value="i" v-for="i in range(1960, 2021)">{{i}}</option>
             </select>
           </div>
         </div>
@@ -41,14 +59,16 @@
           <br>
           <div class="col-md-6 float-left">
             <select v-model="education.end_date.month" class="form-control ">
-              <option value="" selected>Month</option>
-              <option value="" v-for="month in months">{{month}}</option>
+              <option disabled selected>Month</option>
+              <option :value="'gosterme'">Gösterme</option>
+              <option :value="month" v-for="month in months">{{month}}</option>
             </select>
           </div>
           <div class="col-md-6 float-right">
             <select v-model="education.end_date.year" class="form-control ">
-              <option value="" selected>Year</option>
-              <option value="" v-for="i in range(1960, 2021)">{{i}}</option>
+              <option disabled selected>Year</option>
+              <option :value="'gosterme'">Gösterme</option>
+              <option :value="i" v-for="i in range(1960, 2021)">{{i}}</option>
             </select>
           </div>
         </div>
@@ -62,7 +82,9 @@
         <button @click="addEducation()" class="m-3 btn btn-outline-info">Kaydet</button>
       </p>
       <p>
-        <button class="btn btn-outline-dark btn-block col-md-8 offset-md-2">Başka bir eğitim ekle</button>
+        <button @click="addNewEducation()" class="btn btn-outline-dark btn-block col-md-8 offset-md-2">Başka bir eğitim
+          ekle
+        </button>
       </p>
     </div>
 
@@ -79,17 +101,22 @@
       return {
         months: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık']
         ,
+        formShow: true,
+        isUpdate: {
+          status: false,
+          index: null
+        },
         education: {
           agree: null,
-          city: '',
-          school: '',
+          city: null,
+          school: null,
           starter_date: {
-            month: '',
-            year: ''
+            month: 'Month',
+            year: 'Year'
           },
           end_date: {
-            month: '',
-            year: ''
+            month: 'Month',
+            year: 'Year'
           },
           content: '',
         },
@@ -100,10 +127,69 @@
       range: function (start, end) {
         return Array(end - start + 1).fill().map((_, idx) => start + idx)
       },
-      addEducation(){
+      addEducation() {
+        let query = this.education.agree != null && this.education.city != null && this.education.school != null
+          && this.education.starter_date.month != 'Month' && this.education.starter_date.year != 'Year'
+          && this.education.end_date.month != 'Month' && this.education.end_date.year != 'Year';
+
+        if (query) {
+          /* Tempalte üzerinde gösterirken bir filter yazılacak!!!!! */
+
+          for (var i in this.education.starter_date) {
+            if (this.education.starter_date[i] == 'gosterme') {
+              this.education.starter_date[i] = null;
+            }
+          }
+          for (var i in this.education.end_date) {
+            if (this.education.end_date[i] == 'gosterme') {
+              this.education.end_date[i] = null;
+            }
+          }
+        } else {
+          alert("Lütfen Tüm Alanları Doldurunuz !!!!");
+        }
+
+        if (this.isUpdate.status) {
+          this.educationList[this.isUpdate.index] = this.education;
+          this.formShow = false;
+          this.isUpdate = {
+            status: false,
+            index: null
+          }
+        } else {
+          this.educationList.push(this.education);
+          this.formShow = false;
+
+        }
+
+      },
+      addNewEducation() {
+        this.education = {
+          agree: null,
+          city: null,
+          school: null,
+          starter_date: {
+            month: 'Month',
+            year: 'Year'
+          },
+          end_date: {
+            month: 'Month',
+            year: 'Year'
+          },
+        }
+        this.formShow = true;
 
       }
-
+      ,
+      editEducation(index) {
+        this.education = this.educationList[index];
+        this.formShow = true;
+        this.isUpdate.status = true;
+        this.isUpdate.index = index;
+      },
+      destroyEducation(index) {
+        this.educationList.pop(index);
+      }
 
     }
   }
